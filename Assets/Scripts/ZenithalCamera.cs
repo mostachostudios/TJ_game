fd92b18c87965caef223e3f49a1b8f71003d40f6
Rafael J. Vicente <rafaelj.vicente@gmail.com>
2020-03-02 21:00:27 +0100

    Added classroom scenario and dummy PJ for testing

---
+++ Assets/Scripts/ZenithalCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZenithalCamera : MonoBehaviour
+{
+    public float flatDistance = 5;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(0.0f, flatDistance, -flatDistance);
+        transform.LookAt(player.transform.position);
+    }
+}
